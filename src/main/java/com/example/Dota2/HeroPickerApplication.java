0568c9ceac97b5efd1946bfb9f3e7539bcda284c
Samforquer <samforquer@gmail.com>
2023-04-17 12:30:09 -0400

    Adding more basic structure.

--- src/main/java/com/example/Dota2/HeroPickerApplication.java
+++ src/main/java/com/example/Dota2/HeroPickerApplication.java
@@ -18,10 +18,12 @@ public class HeroPickerApplication {
     private static final String FIELD_DELIMITER = "\\,";
     private static final String FILE_BASE_PATH = "C:\\Users\\samfo\\OneDrive\\Documents\\currentDota2Data.csv";
 
-    private final Scanner keyboard = new Scanner(System.in);
+    private final Scanner userInput = new Scanner(System.in);
+
     public static void main(String[] args) {
         SpringApplication.run(HeroPickerApplication.class, args);
         HeroPickerApplication.initialize();
+
 
     }
 
@@ -49,48 +51,57 @@ public class HeroPickerApplication {
     }
     //
 
-    private void run(){
+    private void run() {
         // perform any if checks here for if the data has loaded in properly, etc..
-        while(true){
+        while (true) {
             printMainMenu();
-            int mainMenuSelection = promptForRoleSelection(keyboard.nextLine()); // ?
-            if(mainMenuSelection == 1){
-                printCarryMenu();
+            int mainMenuSelection = promptForMenuSelection("Enter your selection below: "); // ?
+            // add a check here to make sure its one of the 5 int options.
+            if (mainMenuSelection == 1) {
+                System.out.println("You've selected Hard Carry");
+                printSkillLevelMenu();
             }
 
+        }
     }
+
+    private void printMainMenu() {
+        System.out.println("****** Welcome to the Dota 2 Hero picker app! ******");
+        System.out.println("Please select the role you would like to play: (1-5)");
+        System.out.println("1 = Hard Carry");
+        System.out.println("2 = Midlane");
+        System.out.println("3 = Offlane");
+        System.out.println("4 = Soft Support");
+        System.out.println("5 = Hard Support");
+
     }
-        private void printMainMenu(){
-            System.out.println("****** Welcome to the Dota 2 Hero picker app! ******");
-            System.out.println("Please select the role you would like to play: (1-5)");
-            System.out.println("1 = Carry");
-            System.out.println("2 = Midlane");
-            System.out.println("3 = Offlane");
-            System.out.println("4 = Soft Support");
-            System.out.println("5 = Hard Support");
 
-        }
-        private void printCarryMenu(){
-            System.out.println("Select your skill level: (1-5");
-            System.out.println("");
-        }
-        private void printSkillLevelSelectMenu(){
+    private void printSkillLevelMenu() {
+        System.out.println("Select your skill level: (1-5");
+        System.out.println("1 = Crusader or lower: (< 2000 MMR)");
+        System.out.println("2 = Archon: (~2000-3000 MMR)");
+        System.out.println("3 = Legend:  (~3000-4000 MMR)");
+        System.out.println("4 = Ancient: (~4000-5000 MMR)");
+        System.out.println("5 = Divine/Immortal: (5000+ MMR)");
+    }
 
-        }
+    private void printComplexityLevelMenu() {
+        System.out.println("How complex of a hero would you like to play?");
+        System.out.println("1 = Very simple to play");
+        System.out.println("2 = Moderately difficult to play");
+        System.out.println("3 = Very difficult to play");
+        // Definitely need to phrase this better or add some more clarification. Just a placeholder for now to test
+        // functionality.
+    }
+
     private int promptForMenuSelection(String prompt) {
         System.out.print(prompt);
         int menuSelection;
         try {
-            menuSelection = Integer.parseInt(keyboard.nextLine());
+            menuSelection = Integer.parseInt(userInput.nextLine());
         } catch (NumberFormatException e) {
             menuSelection = -1;
         }
         return menuSelection;
     }
-
-        private int promptForRoleSelection(String prompt){
-            System.out.println();
-
-        return 0; // Change this later
-        }
 }
