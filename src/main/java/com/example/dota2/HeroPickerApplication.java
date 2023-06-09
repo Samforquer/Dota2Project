191933e4d6b406adf2f579c80b07c4a60d6132c4
Samforquer <samforquer@gmail.com>
2023-06-09 15:00:26 -0400

    changing localhost port to 8081

--- src/main/java/com/example/dota2/HeroPickerApplication.java
+++ src/main/java/com/example/dota2/HeroPickerApplication.java
@@ -1,13 +1,22 @@
 package com.example.dota2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class) // I don't need a database connection.
 @SpringBootApplication
 public class HeroPickerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HeroPickerApplication.class, args);
+
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
