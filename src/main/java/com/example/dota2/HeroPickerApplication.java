3fb95fd97305f086f19195597a2d1a8ff3195001
Samforquer <samforquer@gmail.com>
2023-06-12 15:18:43 -0400

    adding HeroFilterService

--- src/main/java/com/example/dota2/HeroPickerApplication.java
+++ src/main/java/com/example/dota2/HeroPickerApplication.java
@@ -1,19 +1,31 @@
 package com.example.dota2;
 
+import com.example.dota2.model.Hero;
+import com.example.dota2.service.HeroApiService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class) // I don't need a database connection.
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class HeroPickerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HeroPickerApplication.class, args);
-
+        // Get the HeroApiService instance from the application context
+        ApplicationContext context = SpringApplication.run(HeroPickerApplication.class, args);
+        HeroApiService heroApiService = context.getBean(HeroApiService.class);
+        System.out.println("****************************************");
+        System.out.println("Welcome to the Dota2 Hero Picker App");
+        System.out.println("****************************************");
+        System.out.println(" ");
+        System.out.println(" ");
+        // Call the run method to print out all the heroes
+        HeroPickerApplication heroPickerApplication = new HeroPickerApplication();
+        heroPickerApplication.run(heroApiService);
     }
 
     @Bean
@@ -21,4 +33,10 @@ public class HeroPickerApplication {
         return new RestTemplate();
     }
 
+    public void run(HeroApiService heroApiService) {
+        Hero[] heroes = heroApiService.fetchHeroesFromApi();
+        for (Hero hero : heroes) {
+            System.out.println(hero);
+        }
+    }
 }
--- src/main/java/com/example/dota2/service/HeroFilterService.java
+++ src/main/java/com/example/dota2/service/HeroFilterService.java
@@ -1,10 +1,17 @@
 package com.example.dota2.service;
 
-public class WinRateCalculator {
+import com.example.dota2.model.Hero;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeroFilterService {
     public double calculateWinRate(double pickCount, double winCount) {
         if (pickCount == 0) {
             return 0.0; // Avoid division by zero
         }
         return (winCount / pickCount) * 100;
     }
+
+
 }
