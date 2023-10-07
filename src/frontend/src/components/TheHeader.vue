aa7bde4cecff98e01c58c6b96ace80060d708a98
Samforquer <samforquer@gmail.com>
2023-10-06 23:20:02 -0400

    changed header image

--- src/frontend/src/components/TheHeader.vue
+++ src/frontend/src/components/TheHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <div class="header-content">
       <div id="img">
-        <img class="header-img" src="https://pbs.twimg.com/profile_images/1478893871199186945/1mA6tezL_400x400.jpg" alt="header-img"/>
+        <img class="header-img" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/444903e3-91cb-4e2c-a001-aef2e9d99790/daj7nti-d6776829-738d-49ab-bf2d-ec3aa51d45c8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ0NDkwM2UzLTkxY2ItNGUyYy1hMDAxLWFlZjJlOWQ5OTc5MFwvZGFqN250aS1kNjc3NjgyOS03MzhkLTQ5YWItYmYyZC1lYzNhYTUxZDQ1YzgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.05SBxWgsDERmmCdLOAetZQ_HV70DqMbc6uQ_13vicrI" alt="header-img"/>
       </div>
       <h1>Dota 2 Hero Picker</h1>
     </div>
